ba2113c2953ada41ff66ecf197f7453348c169e3
topku99 <songpholtopku@gmail.com>
2026-08-02 03:10:37 +0700

    Add Download Resume button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,6 +46,10 @@ export default function Home() {
             </ul>
             <div className="flex gap-4">
               <a href="mailto:songpholtopku@gmail.com" className="btn btn-primary">Contact Me</a>
+              <a href="/My_Portfolio/Resume_Songphol.pdf" target="_blank" className="btn btn-outline" style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+                <svg width="20" height="20" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"></path></svg>
+                Download Resume
+              </a>
               {/* <a href="https://github.com/topku99" target="_blank" rel="noreferrer" className="btn btn-outline">GitHub Profile</a> */}
             </div>
           </div>
